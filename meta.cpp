63604a23cbf02b29cf5664ee519e95390689c9dc
Jesper Stefansson <jesper.stefansson@gmail.com>
2024-07-10 22:46:49 +0200

    aaand one more time

--- meta.cpp
+++ meta.cpp
@@ -1426,7 +1426,7 @@ bool generate_header(const char *out_path, const char *src_path, CXTranslationUn
             } else {
                 for (auto decl : procs[i]) {
                     char *short_name = decl.name;
-                    if (char *underline = strrchr(decl.name, '_'); underline) {
+                    if (const char *underline = strrchr(decl.name, '_'); underline) {
                         size_t len = size_t(underline)-size_t(decl.name);
                         short_name = (char*)malloc(len+1);
                         memcpy(short_name, decl.name, len);
